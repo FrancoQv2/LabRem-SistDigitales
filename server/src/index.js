ece6c2847d8817ec8f4f472ec5daa6e5ce7de1cd
Franco Quevedo <francoq96@gmail.com>
2023-05-21 17:09:54 -0300

    Refactorizado de la conexión a la DB

--- server/src/index.js
+++ server/src/index.js
@@ -1,48 +1,39 @@
-import * as dotenv from "dotenv";
+import * as dotenv from "dotenv"
 // Toma las variables configuradas por ENV dentro del docker-compose / dockerfile
-// dotenv.config({ path: './.env'});
-dotenv.config();
+// dotenv.config({ path: './.env'})
+dotenv.config()
 
-import expressServer from "express";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import cors from "cors";
-import Sequelize from "sequelize";
+import expressServer from "express"
+import bodyParser from "body-parser"
+import morgan from "morgan"
+import cors from "cors"
 
-import digital from "./routes/digital.routes.js";
+import { dbConnection } from "./db/dbconfig.js"
 
-const app = expressServer();
-const PORT = process.env.SERVER_PORT || 3000;
+import digital from "./routes/digital.routes.js"
 
-//Necesitamos body-parser para formatear los post en express
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
+const app = expressServer()
+const PORT = process.env.SERVER_PORT || 3000
 
-app.use("/api/digital", digital);
+// Necesitamos body-parser para formatear los post en express
+app.use(morgan("dev"))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
 
-// app.use("/public-key", "id_rsa.pub") // formato x.509
+app.use("/api/digital", digital)
 
 // ---------------------------------------------------------------
 
 // Levantamos el servidor para que escuche peticiones
 app.listen(PORT, () => {
-  console.log(`Server on container port ${PORT}`);
-  console.log(`Server on localhost port ${process.env.LOCALHOST_PORT}`);
-  console.log("----------------------");
-});
+  console.log(`Server on container port ${PORT}`)
+  console.log(`Server on localhost port ${process.env.LOCALHOST_PORT}`)
+  // console.log("----------------------")
+})
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-  }
-);
 export function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise(resolve => setTimeout(resolve, time))
 } 
+
+export const db = dbConnection
