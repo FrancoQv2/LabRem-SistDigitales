d579b8fc0b3ad5ccf65f8d2e8508cf707afae33c
Franco Quevedo <francoq96@gmail.com>
2023-05-29 18:48:40 -0300

    Refactorizado de métodos de UART e I2C

--- server/src/controllers/i2c.controller.js
+++ server/src/controllers/i2c.controller.js
@@ -1,107 +1,102 @@
-import { sequelize, delay} from "../index.js";
-import axios from "axios";
+import { db } from "../index.js"
 
-const idLaboratorio = 2;
+import { delay } from "../lib/delay.js"
+import axios from "axios"
 
-const i2cController = {};
+const idLaboratorio = 2
+const URL_ARDUINO = 'http://192.168.100.75:3031/api/control/arduino'//cambiar por ip arduino
 
-/**
- * -----------------------------------------------------
- * Function - postLabI2C
- * -----------------------------------------------------
- */
-i2cController.postEnsayoI2C = async (req, res) => {
-  const {
-    idUsuario, 
-    frecuencia,
-    memoria,
-    accion, //0 lectura, 1 escritura
-    datos 
-  } = req.body;
+const queries = {
+    getEnsayosI2C: "CALL sp_dameEnsayosI2C();",
+    postEnsayoI2C: "CALL sp_crearEnsayo(:idUsuario,:datosEntrada,:datosSalida,:idLaboratorio);"
+}
 
-  if (frecuencia < 0) {
-    res.status(400).json("la frecuencia es negativa");
-  } else if (memoria < 0) {
-    res.status(400).json("la direccion de memoria no es valida");
-  } else {
-    const url='http://192.168.100.75:3031/api/control/arduino';//cambiar por ip arduino
-    const body={
-      "Estado" : [3,false,true],
-      "Analogico" : [1,1,1]
-    };
-    let respuestaGet;
-    let Msj='';
-    try {
-      let i=0;
-      do {
-        respuestaGet = await axios.get(`${url}/${i}`);
-        await delay(3000);
-        i = i+1;
-      } while (respuestaGet.data.Estado[2]);
-      switch (respuestaGet.data.Error) {
-        case 0:
-          
-          Msj="laboratorio ok";
-          break;
-        case 1:
-          Msj="Error en el angulo limite de azimut";
-          break;
-        case 2:
-          Msj="Error en el angulo limite de elevacion";
-          break;
-        default:
-          Msj="Error de laboratorio incorrecto";
-          break;
-      }
-      res.status(200).json(Msj);
-     } catch (error) {
-       console.error("-> ERROR postensayoI2C:", error);
-     }
-   }
-};
+const i2cController = {}
 
-i2cController.postEnsayoI2CSave = (req, res) => {
-  const {
-    idUsuario, 
-    frecuencia,
-    memoria,
-    accion, //0 lectura, 1 escritura
-    datos 
-  } = req.body;
+// -----------------------------------
+// Métodos GET
+// -----------------------------------
 
-  if (frecuencia < 0) {
-    res.status(400).json("la frecuencia es negativa");
-  } else if (memoria < 0) {
-    res.status(400).json("la direccion de memoria no es valida");
-  } else {
+i2cController.getEnsayosI2C = async (req, res) => {
+    console.log("--------------------")
+    console.log(`--> getEnsayosI2C - ${JSON.stringify(req.params)}`)
 
-    const datosEntrada = {
-      frecuencia: frecuencia,
-      memoria: memoria,
-      accion: accion,
-      datos,datos
-    };
+    const data = await db.query(
+        queries.getEnsayosI2C
+    )
 
-    const datosSalida = {
-    };
-    
-    try {
-      sequelize.query(
-        "CALL sp_crearEnsayo (:idUsuario,:datosEntrada,:datosSalida,:idLaboratorio);",
-        {
-          replacements: {
-            idUsuario: idUsuario,
-            datosEntrada: JSON.stringify(datosEntrada),
-            datosSalida: JSON.stringify(datosSalida),
-            idLaboratorio: idLaboratorio,
-          }
+    let dataParsed = []
+    data.map((ensayo) => {
+        const newEnsayo = {}
+        newEnsayo.Usuario   = ensayo.idUsuario
+        newEnsayo.Fecha     = ensayo.Fecha
+        newEnsayo.Hora      = ensayo.Hora
+        // newEnsayo.velocidad  = ensayo.datosEntrada.velocidad
+        // newEnsayo.pulsadores = ensayo.datosEntrada.pulsadores
+        // newEnsayo.mensaje    = ensayo.datosEntrada.mensaje
+        newEnsayo.accion        = ensayo.datosEntrada.accion
+        newEnsayo.frecuencia    = `${ensayo.datosEntrada.frecuencia} KHz`
+        newEnsayo.direccion     = ensayo.datosEntrada.direccion
+        newEnsayo.datos         = ensayo.datosEntrada.datos
+        dataParsed.push(newEnsayo)
+    })
+
+    await res.status(200).send(dataParsed)
+}
+
+// -----------------------------------
+// Métodos POST
+// -----------------------------------
+
+i2cController.postEnsayoI2C = (req, res) => {
+    console.log(`-\n--> postEnsayoI2C - ${JSON.stringify(req.body)}\n---`)
+
+    const {
+        idUsuario,
+        accion,         // Lectura, Escritura
+        frecuencia,     // 100 KHz, 400 KHz, 1000 KHz, 
+        direccion,      // Notación 0x
+        datos
+    } = req.body
+
+    if (
+        frecuencia != 100 && 
+        frecuencia != 400 && 
+        frecuencia != 1000
+    ) {
+        res.status(400).json("La frecuencia no es válida!")
+    } else if (direccion < 0) {
+        res.status(400).json("La dirección de memoria no es válida!")
+    } else {
+
+        const datosEntrada = {
+            accion:     accion,
+            frecuencia: frecuencia,
+            direccion:  `0x${direccion.toUpperCase()}`,
+            datos:      datos
         }
-      );
-      res.status(200).json("guardado en base de datos");
-    } catch (error) {
-      console.error("-> ERROR postEnsayoI2CSave:", error);
-    }
-  }
-};
 
-export { i2cController };
+        const datosSalida = {
+            // indicadores: pulsadores
+        }
+
+        try {
+            db.query(
+                queries.postEnsayoI2C,
+                {
+                    replacements: {
+                        idUsuario: idUsuario,
+                        datosEntrada: JSON.stringify(datosEntrada),
+                        datosSalida: JSON.stringify(datosSalida),
+                        idLaboratorio: idLaboratorio,
+                    }
+                }
+            )
+            res.status(200).json("guardado en base de datos")
+        } catch (error) {
+            console.error("-> ERROR postEnsayoI2CSave:", error)
+        }
+    }
+}
+
+export { i2cController }
--- server/src/controllers/uart.controller.js
+++ server/src/controllers/uart.controller.js
@@ -1,136 +1,118 @@
-import { sequelize, delay } from "../index.js";
-import axios from "axios";
-const idLaboratorio = 1;
+import { db } from "../index.js"
 
-const uartController = {};
+import { delay } from "../lib/delay.js"
+import axios from "axios"
 
-/**
- * -----------------------------------------------------
- * Function - postLabUART
- * -----------------------------------------------------
- */
-uartController.postEnsayoUART = async (req, res) => {
-  const {
-    idUsuario,
-    velocidad,
-    pulsadores,
-    mensaje
-  } = req.body;
+const idLaboratorio = 1
 
-  if ( velocidad != 300    &&
-       velocidad != 600    &&
-       velocidad != 1200   &&
-       velocidad != 2400   &&
-       velocidad != 4800   &&
-       velocidad != 9600   &&
-       velocidad != 19200  &&
-       velocidad != 38400  &&
-       velocidad != 57600  &&
-       velocidad != 115200 &&
-       velocidad != 230400 &&
-       velocidad != 460800 &&
-       velocidad != 921600 ) {
-    console.log("la velocidad seteada no es uno de los valores validos");
-    res.status(400).json("la velocidad seteada no es uno de los valores validos");
-  } else {
-    const url='http://192.168.100.75:3031/api/control/arduino';//cambiar por ip arduino
-    const body={
-      "Estado" : [3,false,true],
-      "Analogico" : [1,1,1]
-    };
-    let respuestaGet;
-    let Msj='';
-   try {
-     let i=0;
-     do {
-       respuestaGet = await axios.get(`${url}/${i}`);
-       await delay(3000);
-       i = i+1;
-     } while (respuestaGet.data.Estado[2]);
-     switch (respuestaGet.data.Error) {
-       case 0:
-         
-         Msj="laboratorio ok";
-         break;
-       case 1:
-         Msj="Error en el angulo limite de azimut";
-         break;
-       case 2:
-         Msj="Error en el angulo limite de elevacion";
-         break;
-       default:
-         Msj="Error de laboratorio incorrecto";
-         break;
-     }
-     res.status(200).json(Msj);
+const queries = {
+    getEnsayosUART: "CALL sp_dameEnsayosUART();",
+    postEnsayoUART: "CALL sp_crearEnsayo(:idUsuario, :datosEntrada, :datosSalida, :idLaboratorio);"
+}
+
+const uartController = {}
+
+// -----------------------------------
+// Métodos GET
+// -----------------------------------
+
+uartController.getEnsayosUART = async (req, res) => {
+    console.log("--------------------")
+    console.log(`--> getEnsayosUART - ${JSON.stringify(req.params)}`)
+
+    try {        
+        const data = await db.query(
+            queries.getEnsayosUART
+        )
+        
+        let dataParsed = []
+        data.map((ensayo) => {
+            const newEnsayo = {}
+            newEnsayo.Usuario       = ensayo.idUsuario
+            newEnsayo.Fecha         = ensayo.Fecha
+            newEnsayo.Hora          = ensayo.Hora
+            newEnsayo.velocidad     = ensayo.datosEntrada.velocidad
+            newEnsayo.pulsadores    = ensayo.datosEntrada.pulsadores
+            newEnsayo.mensaje       = ensayo.datosEntrada.mensaje
+            dataParsed.push(newEnsayo)
+        })
+        
+        await res.status(200).send(dataParsed)
     } catch (error) {
-      console.error("-> ERROR postensayoUART:", error);
+        res.status(404).send("No hay ensayos realizados en este laboratorio!")
     }
-  }
-};
+}
 
-/**
- * -----------------------------------------------------
- * Function - postLabUART
- * -----------------------------------------------------
- */
-uartController.postEnsayoUARTSave = (req, res) => {
-  const {
-    idUsuario,
-    velocidad,
-    cantidadBitDato,
-    paridad,// false par, true impar
-    cantidadBitParada,
-    mensaje
-  } = req.body;
+// -----------------------------------
+// Métodos POST
+// -----------------------------------
 
-  if ( velocidad != 300    &&
-       velocidad != 600    &&
-       velocidad != 1200   &&
-       velocidad != 2400   &&
-       velocidad != 4800   &&
-       velocidad != 9600   &&
-       velocidad != 19200  &&
-       velocidad != 38400  &&
-       velocidad != 57600  &&
-       velocidad != 115200 &&
-       velocidad != 230400 &&
-       velocidad != 460800 &&
-       velocidad != 921600 ) {
-    console.log("la velocidad seteada no es uno de los valores validos");
-    res.status(400).json("la velocidad seteada no es uno de los valores validos");
-  } else if ( cantidadBitDato < 1 ) {
-    res.status(400).json("la cantidad de bits del dato es 0 o negativo");
-  } else if ( cantidadBitParada != 0 && cantidadBitParada != 1 && cantidadBitParada != 2) {
-res.status(400).json("la cantidad de bit de parada no es 0, 1 o 2");
-  } else {
+uartController.postEnsayoUART = (req, res) => {
+    const {
+        idUsuario,
+        velocidad,
+        bitsDatos,
+        paridad,
+        bitsParada,
+        pulsadores,
+        mensaje,
+    } = req.body
 
-    const datosEntrada = {
-      velocidad: velocidad,
-      cantidadBitDato: cantidadBitDato,
-      paridad: paridad,
-      cantidadBitParada: cantidadBitParada,
-      mensaje: mensaje
-    };
-    const datosSalida = {
-    };
-    try {
-      sequelize.query(
-        "CALL sp_crearEnsayo (:idUsuario,:datosEntrada,:datosSalida,:idLaboratorio);",
-        {
-          replacements: {
-            idUsuario: idUsuario,
-            datosEntrada: JSON.stringify(datosEntrada),
-            datosSalida: JSON.stringify(datosSalida),
-            idLaboratorio: idLaboratorio,
-          }
+    console.log(req.body);
+
+    if (velocidad != 300    &&
+        velocidad != 600    &&
+        velocidad != 1200   &&
+        velocidad != 2400   &&
+        velocidad != 4800   &&
+        velocidad != 9600   &&
+        velocidad != 19200  &&
+        velocidad != 38400  &&
+        velocidad != 57600  &&
+        velocidad != 115200 &&
+        velocidad != 230400 &&
+        velocidad != 460800 &&
+        velocidad != 921600
+    ) {
+        console.log("La velocidad seteada no es válida")
+        res.status(400).json("La velocidad seteada no es válida")
+    } else if (!Array.isArray(pulsadores) || pulsadores.length !== 4){
+        res.status(400).json("Los datos de los pulsadores no son válidos")
+    } else {
+        const datosEntrada = {
+            velocidad:  velocidad,
+            bitsDatos:  bitsDatos,
+            paridad:    paridad,
+            bitsParada: bitsParada,
+            pulsadores: pulsadores,
+            mensaje:    mensaje,
+            pulsadores: pulsadores,
+            mensaje:    mensaje
         }
-      );
-      res.status(200).json("guardado en base de datos");
-    } catch (error) {
-      console.error("-> ERROR postEnsayoUARTSave:", error);
-    }
-  }
-};
 
-export { uartController };
+        const datosSalida = {
+            indicadores: pulsadores
+        }
+
+        try {
+            db.query(
+                queries.postEnsayoUART,
+                {
+                    replacements: {
+                        idUsuario:      idUsuario,
+                        datosEntrada:   JSON.stringify(datosEntrada),
+                        datosSalida:    JSON.stringify(datosSalida),
+                        idLaboratorio:  idLaboratorio
+                    }
+                }
+            )
+
+            res.status(200).json({ msg: "Parámetros correctos. Guardado en DB" })
+        } catch (error) {
+            console.error("-> ERROR postEnsayoUART:", error)
+            res.status(500).json({ msg: "Error en postEnsayoUART!" })
+        }
+    }
+}
+
+export { uartController }
