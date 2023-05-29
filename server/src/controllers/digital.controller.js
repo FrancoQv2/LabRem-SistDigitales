42169071713d815d01fa843616b3331a524ba9a0
Franco Quevedo <francoq96@gmail.com>
2023-05-29 18:48:18 -0300

    Refactorizado de métodos de Laboratorios y Ensayos

--- server/src/controllers/digital.controller.js
+++ server/src/controllers/digital.controller.js
@@ -1,260 +1,291 @@
-import { sequelize } from "../index.js";
+import { db } from "../index.js"
 
-const digitalController = {};
+const digitalController = {}
 
-/**
- * -----------------------------------------------------
- * Function - getLaboratorios
- * -----------------------------------------------------
- */
+const queries = {
+    getLaboratorios: "CALL sp_dameLaboratorios();",
+    getEnsayos: "CALL sp_dameEnsayos(:idLaboratorio);",
+
+    getLaboratorio: "CALL sp_dameLaboratorio(:idLaboratorio);",
+    getEnsayosUsuario: "CALL sp_dameEnsayosUsuario(:idLaboratorio,:idUsuario);",
+  
+    postLaboratorio: "CALL sp_crearLaboratorio(:nombre,:descripcion);",
+    updateLaboratorio: "CALL sp_modificarLaboratorio(:idLaboratorio,:area,:nombre,:descripcion);",
+
+    deleteLaboratorio: "CALL sp_borrarLaboratorio(:idLaboratorio);",
+    deleteEnsayo: "CALL sp_borrarEnsayo(:idEnsayo);"
+}
+
+// -----------------------------------
+// Métodos GET
+// -----------------------------------
+
 digitalController.getLaboratorios = async (req, res) => {
-  const response = await sequelize.query("CALL sp_dameLaboratorios();");
-  console.log(typeof response);
-  console.log(response);
+    console.log("--------------------")
+    console.log(`--> getLaboratorios - ${JSON.stringify(req.params)}`)
 
-  await res.send(response);
-};
+    try {
+        const data = await db.query(
+            queries.getLaboratorios
+        )
 
-
-/**
- * -----------------------------------------------------
- * Function - getLaboratorioById
- * -----------------------------------------------------
- */
-digitalController.getLaboratorioById = async (req, res) => {
-  const { idLaboratorio } = req.params;
-
-  const response = await sequelize.query(
-    "CALL sp_dameLaboratorio(:idLaboratorio);",
-    {
-      replacements: {
-          idLaboratorio: idLaboratorio
-      } 
+        if (!data.length) {
+            await res.status(404).send("No existen laboratorios!")
+        } else {
+            await res.status(200).send(data)
+        }
+    } catch (error) {
+        console.error("-> ERROR getLaboratorios:", error)
+        await res.status(500).send('Error en getLaboratorios!')
     }
-  );
-  
-  await res.send(response[0]);
-};
+}
 
+digitalController.getLaboratorio = async (req, res) => {
+    console.log("--------------------")
+    console.log(`--> getLaboratorio - ${JSON.stringify(req.params)}`)
 
-/**
- * -----------------------------------------------------
- * Function - getEnsayosUsuario
- * -----------------------------------------------------
- */
-digitalController.getEnsayosUsuario = async (req, res) => {
-  console.log(req.params);
-    
-  const { idLaboratorio, idUsuario } = req.params;
+    const { idLaboratorio } = req.params
 
-  const response = await sequelize.query(
-    "CALL sp_dameEnsayo(:idUsuario,:idLaboratorio);",
-    {
-      replacements: {
-        idLaboratorio: idLaboratorio,
-        idUsuario: idUsuario,
-      }
+    try {
+        const data = await db.query(
+            queries.getLaboratorio,
+            {
+                replacements: {
+                    idLaboratorio: idLaboratorio
+                }
+            }
+        )
+
+        if (!data.length) {
+            await res.status(404).send("No existe el laboratorio buscado!")
+        } else {
+            await res.status(200).send(data[0])
+        }
+    } catch (error) {
+        console.error("-> ERROR getLaboratorio:", error)
+        await res.status(500).send('Error en getLaboratorio!')
     }
-  );
+}
 
-  let dataParsed = [];
-  
-  if (idLaboratorio == 1) {//uart
-    response.map((ensayo)=>{
-      const newEnsayo = {}
-      newEnsayo.Fecha = ensayo.Fecha
-      newEnsayo.Hora = ensayo.Hora
-      newEnsayo.velocidad = ensayo.datosEntrada.velocidad
-      newEnsayo.paridad = ensayo.datosEntrada.paridad
-      newEnsayo.cantidadBitDato = ensayo.datosEntrada.cantidadBitDato
-      newEnsayo.cantidadBitParada = ensayo.datosEntrada.cantidadBitParada
-      newEnsayo.mensaje = ensayo.datosEntrada.mensaje
-      dataParsed.push(newEnsayo)
-    })
-  } else if (idLaboratorio == 2) {
-    response.map((ensayo)=>{
-      const newEnsayo = {}
-      newEnsayo.Fecha = ensayo.Fecha
-      newEnsayo.Hora = ensayo.Hora
-      newEnsayo.frecuencia = ensayo.datosEntrada.frecuencia
-      newEnsayo.memoria = ensayo.datosEntrada.memoria
-      newEnsayo.accion = ensayo.datosEntrada.accion
-      newEnsayo.datos = ensayo.datosEntrada.datos
-      dataParsed.push(newEnsayo)
-    })
-  }
-  console.log("--------------------------------");
-  console.log(response);
-  console.log("--------------------------------");
-  
-  await res.send(JSON.stringify(dataParsed));
-};
-
-
-/**
- * @param {number} idEnsayo
- * @return {object} informacion de un ensayo en particular
- */
-digitalController.getDeleteEnsayo = async (req, res) => {
-  const { idEnsayo } = req.params;
-
-  const response = await sequelize.query(
-    "CALL sp_borrarEnsayo(:idEnsayo);",
-    {
-      replacements: {
-        idEnsayo: idEnsayo
-      } 
-    }
-  );
-  
-  await res.send(response[0]);
-};
-
-/**
- * @param {number} idLaboratorio
- * @return {object} informacion de un laboratorio en particular
- */
-digitalController.getDeleteLaboratorio = async (req, res) => {
-  const { idLaboratorio } = req.params;
-
-  const response = await sequelize.query(
-    "CALL sp_borrarLaboratorio(:idLaboratorio);",
-    {
-      replacements: {
-        idLaboratorio: idLaboratorio
-      } 
-    }
-  );
-  
-  await res.send(response[0]);
-};
-
-/**
- * @param {number} idLaboratorio
- * @return {object} informacion de un laboratorio en particular
- */
 digitalController.getEnsayos = async (req, res) => {
-  const { idLaboratorio } = req.params;
+    console.log("--------------------")
+    console.log(`--> getEnsayos - ${JSON.stringify(req.params)}`)
 
-  const response = await sequelize.query(
-    "CALL sp_dameEnsayos(:idLaboratorio);",
-    {
-      replacements: {
-        idLaboratorio: idLaboratorio
-      } 
-    }
-  );
-  
-  let dataParsed = [];
-  
-  if (idLaboratorio == 1) {//uart
-    response.map((ensayo)=>{
-      const newEnsayo = {}
-      newEnsayo.Fecha = ensayo.Fecha
-      newEnsayo.Hora = ensayo.Hora
-      newEnsayo.velocidad = ensayo.datosEntrada.velocidad
-      newEnsayo.paridad = ensayo.datosEntrada.paridad
-      newEnsayo.cantidadBitDato = ensayo.datosEntrada.cantidadBitDato
-      newEnsayo.cantidadBitParada = ensayo.datosEntrada.cantidadBitParada
-      newEnsayo.mensaje = ensayo.datosEntrada.mensaje
-      dataParsed.push(newEnsayo)
-    })
-  } else if (idLaboratorio == 2) {
-    response.map((ensayo)=>{
-      const newEnsayo = {}
-      newEnsayo.Fecha = ensayo.Fecha
-      newEnsayo.Hora = ensayo.Hora
-      newEnsayo.frecuencia = ensayo.datosEntrada.frecuencia
-      newEnsayo.memoria = ensayo.datosEntrada.memoria
-      newEnsayo.accion = ensayo.datosEntrada.accion
-      newEnsayo.datos = ensayo.datosEntrada.datos
-      dataParsed.push(newEnsayo)
-    })
-  }
-  console.log("--------------------------------");
-  console.log(response);
-  console.log("--------------------------------");
-  
-  await res.send(JSON.stringify(dataParsed));
-};
-
-/**
- * -----------------------------------------------------
- * Function - postLab
- * -----------------------------------------------------
- */
-digitalController.postLab = (req, res) => {
-  const { area, nombre, imagen, descripcion} = req.body;
-
-  if (area == null) {
-    res.status(400).json("Area nula");
-  } else if (nombre == null) {
-    res.status(400).json("nombre nulo");
-  }  else if (descripcion == null) {
-    res.status(400).json("Descripcion nula");
-  }else {    
-    try {
-      sequelize.query(
-        "CALL sp_crearLaboratorio (:area,:nombre,:imagen,:descripcion);",
-        {
-          replacements: {
-            area: area,
-            nombre: nombre,
-            imagen: imagen,
-            descripcion: descripcion,
-          }
-        }
-      );
-      res.status(200).json("Parámetros correctos");
-    } catch (error) {
-      console.error("-> ERROR postLab:", error);
-    }
-  }
-};
-
-/**
- * -----------------------------------------------------
- * Function - postModLab
- * -----------------------------------------------------
- */
-digitalController.postModLab = async (req, res) => {
-  const {codLaboratorio, area, nombre, imagen, descripcion} = req.body;
-  const respuesta = await sequelize.query(
-    "CALL sp_dameLaboratorio(:codLaboratorio)",
-    {
-      replacements: {
-        codLaboratorio: codLaboratorio
-      }
-    }
-  );
-  if (respuesta[0]==null){
-    res.status(400).json("codigo no asociado a ningun laboratorio existente");
-  } else if (area == null) {
-    res.status(400).json("Area nula");
-  } else if (nombre == null) {
-    res.status(400).json("nombre nulo");
-  }  else if (descripcion == null) {
-    res.status(400).json("Descripcion nula");
-  }else { 
+    const { idLaboratorio } = req.params
 
     try {
-      sequelize.query(
-        "CALL sp_modificarLaboratorio (:codLaboratorio,:area,:nombre,:imagen,:descripcion);",
-        {
-          replacements: {
-            codLaboratorio: codLaboratorio,
-            area: area,
-            nombre: nombre,
-            imagen: imagen,
-            descripcion: descripcion,
-          }
-        }
-      );
-      res.status(200).json("modificado correctamente");
-    } catch (error) {
-      console.error("-> ERROR postLab:", error);
-    }
-  }
-};
+        const data = await db.query(
+            queries.getEnsayos,
+            {
+                replacements: {
+                    idLaboratorio: idLaboratorio
+                }
+            }
+        )
 
-export { digitalController };
+        if (!data.length) {
+            await res.status(404).send("No existen ensayos para este laboratorio!")
+        } else {
+            await res.status(200).send(data)
+        }
+    } catch (error) {
+        console.error("-> ERROR getEnsayos:", error)
+        await res.status(500).send('Error en getEnsayos!')
+    }
+}
+
+digitalController.getEnsayosUsuario = async (req, res) => {
+    console.log("--------------------")
+    console.log(`--> getEnsayosUsuario - ${JSON.stringify(req.params)}`)
+
+    const { idLaboratorio, idUsuario } = req.params
+
+    try {
+        const data = await db.query(
+            queries.getEnsayosUsuario,
+            {
+                replacements: {
+                    idLaboratorio: idLaboratorio,
+                    idUsuario: idUsuario,
+                }
+            }
+        )
+
+        console.log(data);
+
+        if (!data.length) {
+            await res.status(404).send("No existen ensayos realizados por este alumno para este laboratorio!")
+        } else {
+            let dataParsed = []
+
+            if (idLaboratorio == 1) {
+                data.map((ensayo, index) => {
+                    const newEnsayo = {}
+                    newEnsayo.index = index + 1
+                    newEnsayo.Fecha = ensayo.Fecha
+                    newEnsayo.Hora  = ensayo.Hora
+                    newEnsayo.velocidad  = ensayo.datosEntrada.velocidad
+                    newEnsayo.bitsDatos  = ensayo.datosEntrada.bitsDatos
+                    newEnsayo.bitsParada = ensayo.datosEntrada.bitsParada
+                    newEnsayo.paridad    = ensayo.datosEntrada.paridad
+                    newEnsayo.pulsadores = ensayo.datosEntrada.pulsadores
+                    newEnsayo.mensaje    = ensayo.datosEntrada.mensaje
+                    dataParsed.push(newEnsayo)
+                })
+            } else if (idLaboratorio == 2) {
+                data.map((ensayo, index) => {
+                    const newEnsayo = {}
+                    newEnsayo.index = index + 1
+                    newEnsayo.Fecha = ensayo.Fecha
+                    newEnsayo.Hora  = ensayo.Hora
+                    // newEnsayo.velocidad     = ensayo.datosEntrada.velocidad
+                    // newEnsayo.pulsadores    = ensayo.datosEntrada.pulsadores
+                    // newEnsayo.mensaje       = ensayo.datosEntrada.mensaje
+                    newEnsayo.accion        = ensayo.datosEntrada.accion
+                    newEnsayo.frecuencia    = (ensayo.datosEntrada.frecuencia != 1000) ? `${ensayo.datosEntrada.frecuencia} KHz` : `${ensayo.datosEntrada.frecuencia/1000} MHz`
+                    newEnsayo.direccion     = ensayo.datosEntrada.direccion
+                    newEnsayo.datos         = ensayo.datosEntrada.datos
+                    dataParsed.push(newEnsayo)
+                })
+            }
+
+            await res.status(200).json(dataParsed)
+        }
+    } catch (error) {
+        console.error("-> ERROR getEnsayosUsuario:", error)
+        await res.status(500).send('Error en getEnsayosUsuario!')
+    }
+}
+
+// -----------------------------------
+// Métodos POST
+// -----------------------------------
+
+digitalController.postLaboratorio = (req, res) => {
+    const { nombre, descripcion } = req.body
+
+    if (nombre == null || nombre == "") {
+        res.status(400).json("El nombre no puede estar vacío!")
+    } else if (descripcion == null || descripcion == "") {
+        res.status(400).json("La descripción no puede estar vacía!")
+    } else {
+        try {
+            db.query(
+                queries.postLaboratorio,
+                {
+                    replacements: {
+                        nombre: nombre,
+                        descripcion: descripcion,
+                    }
+                }
+            )
+
+            res.status(200).json(`Laboratorio '${nombre}' creado exitosamente!`)
+        } catch (error) {
+            console.error("-> ERROR postLaboratorio:", error)
+            res.status(500).send('Error en postLaboratorio!')
+        }
+    }
+}
+
+digitalController.updateLaboratorio = async (req, res) => {
+    console.log("--------------------")
+    console.log(`--> updateLaboratorio - ${JSON.stringify(req.body)}`)
+
+    const { idLaboratorio, area, nombre, descripcion } = req.body
+    
+    const response = await db.query(
+        queries.getLaboratorio,
+        {
+            replacements: {
+                idLaboratorio: idLaboratorio
+            }
+        }
+    )
+
+    if (response[0] == null) {
+        res.status(400).json("Código no asociado a ningun laboratorio existente")
+    } else if (area == null || area == "") {
+        res.status(400).json("El área no puede estar vacía!")
+    } else if (nombre == null || nombre == "") {
+        res.status(400).json("El nombre no puede estar vacío!")
+    } else if (descripcion == null || descripcion == "") {
+        res.status(400).json("La descripción no puede estar vacía!")
+    } else {
+
+        try {
+            db.query(
+                queries.updateLaboratorio,
+                {
+                    replacements: {
+                        idLaboratorio: idLaboratorio,
+                        area: area,
+                        nombre: nombre,
+                        descripcion: descripcion,
+                    }
+                }
+            )
+            res.status(200).json("Laboratorio modificado correctamente!")
+        } catch (error) {
+            console.error("-> ERROR updateLaboratorio:", error)
+            res.status(400).json("Error en updateLaboratorio!")
+        }
+    }
+}
+
+// -----------------------------------
+// Métodos DELETE
+// -----------------------------------
+
+digitalController.deleteLaboratorio = async (req, res) => {
+    console.log("--------------------")
+    console.log(`--> deleteLaboratorio - ${JSON.stringify(req.params)}`)
+
+    const { idLaboratorio } = req.params
+
+    try {
+        const response = await db.query(
+            queries.deleteLaboratorio,
+            {
+                replacements: {
+                    idLaboratorio: idLaboratorio
+                }
+            }
+        )
+    
+        if (!response.length) {
+            await res.status(404).send(response)
+        } else {
+            await res.status(200).send(response)
+        }
+    } catch (error) {
+        console.error("-> ERROR deleteLaboratorio:", error)
+        await res.status(500).send('Error en deleteLaboratorio!')
+    }
+}
+
+digitalController.deleteEnsayo = async (req, res) => {
+    const { idEnsayo } = req.params
+
+    try {
+        const response = await db.query(
+            queries.deleteEnsayo,
+            {
+                replacements: {
+                    idEnsayo: idEnsayo
+                }
+            }
+        )
+    
+        if (!response.length) {
+            await res.status(404).send(response[0])
+        } else {
+            await res.status(200).send(response[0])
+        }
+    } catch (error) {
+        console.error("-> ERROR deleteEnsayo:", error)
+        await res.status(500).send('Error en deleteEnsayo!')
+    }
+}
+
+export { digitalController }
